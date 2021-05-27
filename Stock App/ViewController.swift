ab536efbc7f789ad14079c231eaac2fa1686525b
Thobio Joseph <jthobio2@gmail.com>
2021-05-27 18:07:26 +0530

    search bar set up

--- Stock App/ViewController.swift
+++ Stock App/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchTableViewController: UITableViewController{
+    
+    private lazy var searchController:UISearchController = {
+       let sc = UISearchController(searchResultsController: nil)
+        sc.searchResultsUpdater = self
+        sc.delegate = self
+        sc.obscuresBackgroundDuringPresentation = false
+        sc.searchBar.placeholder = "Enter a company name or symbol"
+        sc.searchBar.autocapitalizationType = .allCharacters
+       return sc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpNavigationBar()
     }
-
+    
+    private func setUpNavigationBar(){
+        navigationItem.searchController = searchController
+    }
 
 }
 
+extension SearchTableViewController: UISearchResultsUpdating,UISearchControllerDelegate {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+}
